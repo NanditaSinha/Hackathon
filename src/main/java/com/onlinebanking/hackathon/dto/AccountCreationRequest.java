7407516d5323c3c795a90abd7184b0af128d62c7
Nandita Sinha <Nandita070250@outlook.com>
2024-07-09 11:57:04 +0200

    Adding Lombok where it was missing

--- src/main/java/com/onlinebanking/hackathon/dto/AccountCreationRequest.java
+++ src/main/java/com/onlinebanking/hackathon/dto/AccountCreationRequest.java
@@ -1,24 +1,12 @@
 package com.onlinebanking.hackathon.dto;
 
+import lombok.Data;
 import java.math.BigDecimal;
 
+
+@Data
 public class AccountCreationRequest {
     private Long accountNumber;
     private BigDecimal balance;
 
-    public Long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(Long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
 }
--- src/main/java/com/onlinebanking/hackathon/service/TransactionService.java
+++ src/main/java/com/onlinebanking/hackathon/service/TransactionService.java
@@ -84,6 +84,5 @@ public class TransactionService {
             return dto;
         }).collect(Collectors.toList());
     }
-
 }
 
